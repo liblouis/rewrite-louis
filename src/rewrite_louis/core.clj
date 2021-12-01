1379f1896c54318d5a000ad98163c2aeb528685a
Christian Egli <christian@egli.cc>
2021-12-01 20:06:28 +0100

    Add a main function that accepts filenames and validates them

--- src/rewrite_louis/core.clj
+++ src/rewrite_louis/core.clj
@@ -1,7 +1,14 @@
 (ns rewrite-louis.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.java.io :as io]
+            [instaparse.core :as insta]))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (let [parser (insta/parser (io/resource "louis.bnf"))]
+    (doseq [f args]
+      (let [result (->> f slurp (insta/parse parser))]
+        (if (insta/failure? result)
+          (println f result)
+          (println f "OK"))))))
